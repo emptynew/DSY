bc7f0bb4aa697de58a3d977c80bafba86484fac8
LC1301304 <LC1301304>
2018-08-16 09:51:49 -0400

    probando github primera subida

---
+++ Ejemplohola/src/ejemplohola/Ejemplohola.java
@@ -0,0 +1,19 @@
+
+package ejemplohola;
+
+/**
+ *
+ * @author Duoc
+ */
+public class Ejemplohola {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola amigos");
+        System.out.println("Este ejemplo es de pana github");
+        // TODO code application logic here
+    }
+    
+}
